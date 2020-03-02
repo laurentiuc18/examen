f110efd0c63ab130fce203c7e96b11234410051b
Lauren <laurentiuciucan@hotmail.es>
2020-03-02 11:29:07 +0100

    Segundo comentario

---
+++ Ejercicio1/MediaPares.java
@@ -0,0 +1,40 @@
+package Ejercicio1;
+
+import java.util.Scanner;
+
+public class MediaPares {
+
+	public static void main(String[] args) {
+		
+		int x=0,y=0,numPar=0,numImpar=0;
+		int []enteros;
+		enteros =new int[10];
+		Scanner sc=new Scanner(System.in);
+		
+		
+		for(int i=0 ; i<10; i++) {
+			
+			System.out.println("Ingresa 10 numeros");
+			enteros[i]=sc.nextInt();
+		}
+		
+		for(int i=0 ; i<10; i++) {
+			
+			if(enteros[i]%2==0) {
+				x++;
+				numPar=numPar+enteros[i];
+			}else {
+				y++;
+			numImpar=numImpar+enteros[i];}
+			
+		}
+		
+		
+		numPar=numPar/x;
+		numImpar=numImpar/y;
+		
+		System.out.print("La media par es " + numPar+" la media impar es "+numImpar);
+		
+	}
+
+}
